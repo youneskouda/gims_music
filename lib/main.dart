241e2987a913c3591bcde2e5eb7231818110415c
youneskouda <110052226+youneskouda@users.noreply.github.com>
2025-06-23 22:47:31 +0100

    Fix play method argument and update NowPlayingPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:audio_service/audio_service.dart';
-
-import 'pages/home_page.dart'; // Your homepage file
+import 'pages/home_page.dart';
 import 'providers/audio_player_provider.dart';
-import 'services/audio_handler.dart'; // You will create this file next
 
-late AudioPlayerHandler audioHandler;
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize audio service
-  audioHandler = await AudioService.init(
-    builder: () => AudioPlayerHandler(),
-    config: const AudioServiceConfig(
-      androidNotificationChannelId: 'com.gims.music.channel.audio',
-      androidNotificationChannelName: 'GIMS Music Playback',
-      androidNotificationOngoing: true,
-    ),
-  );
-
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AudioPlayerProvider()),
-      ],
+    return ChangeNotifierProvider(
+      create: (_) => AudioPlayerProvider(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'GIMS Music',
-        theme: ThemeData.dark(),
-        home: HomePage(),
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        home: const HomePage(),
       ),
     );
   }
